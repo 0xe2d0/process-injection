dda3a1baa6546c5e98f0c795f4c68a38d8872efd
Eyyüb <89468647+0xe2d0@users.noreply.github.com>
2023-10-08 00:33:21 +0300

    Dll Injector Created.

---
+++ dllinjectionn.cpp
@@ -0,0 +1,34 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+HMODULE hKernel32 = NULL;
+
+wchar_t dllPath[MAX_PATH] = L"C:\\xxx\\xxx\\xxx\\randomDLL.dll";
+size_t dllSize = sizeof(dllPath);
+
+int main(int argc, char* argv[]) {
+
+	DWORD PID = atoi(argv[1]);
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
+	
+
+	hKernel32 = GetModuleHandleW(L"kernel32");
+
+	LPTHREAD_START_ROUTINE startThis = (LPTHREAD_START_ROUTINE)GetProcAddress(hKernel32, "LoadLibraryW");
+
+	LPVOID rBuffer = VirtualAllocEx(hProcess, NULL, dllSize, (MEM_COMMIT | MEM_RESERVE), PAGE_READWRITE);
+
+	size_t bff = 0;
+
+	WriteProcessMemory(hProcess, rBuffer, dllPath, dllSize, &bff);
+
+	DWORD TID = NULL;
+
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, startThis, rBuffer, 0, &TID);
+
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+
+	return EXIT_SUCCESS;
+}
